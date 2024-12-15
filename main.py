4bc06a85ad64b3239dd5684c93470de977a8b383
yipeiyo <501299339@qq.com>
2024-12-15 18:18:40 +0800

    modified to fit pbmc, plot and benchmark added

--- main.py
+++ main.py
@@ -1,45 +1,43 @@
 from datetime import datetime
-
-# import os
-# import torch
-# import seaborn as sns
-# import numpy as np
-# import pandas as pd
-# import scanpy as sc
 import mudata as mu
-
-# import matplotlib.pyplot as plt
-# from gmi import Trainer, run_benchmark
-from gmi import GraphMosaicIntegration
-
-# File paths
-# NODES_PATH = "/home/yuyipei/graph_mosaic_integration/data/All_nodes.csv"
-# WEIGHTS_PATH = "/home/yuyipei/graph_mosaic_integration/data/Weights.csv"
-# mdata_path = "/data/share_data/yuytest/gmi_data/MOP.h5mu"
-# Save file paths
-
-# edge_path = "./data/edges.pt"
-# edge_weights_path = "./data/edge_weights.pt"
-# device = "cuda:1" if torch.cuda.is_available() else "cpu"
+import pandas as pd
+from gmi import GraphMosaicIntegration,run_benchmark, data_infor_integrate,plot_umap
 
 
-# if __name__ == "__main__":
-#     mdata_path = "/data/share_data/yuytest/gmi_data/MOP.h5mu"
-#     for i in ['non_domain']:
-#         result = f"/home/yuyipei/graph_mosaic_integration/result/{i}"
-#         train_model(i,result)
-#         evaluate_trained_embeddings(i,result,mdata_path)
-#         plot_ablation()
+mdata_path = "/data/share_data/yuytest/gmi_data/pbmc.h5mu"
+mdata = mu.read(mdata_path)
+result_path = f"./result/{datetime.now().strftime('%Y-%m-%d_%H-%M')}"
+# 获取完整的batch和标签，并保存在batch和标签列
+mdata = data_infor_integrate(mdata, feature_key="batch",saved_feature_name="batch", target_attr="obs")
+mdata = data_infor_integrate(mdata, feature_key="coarse_cluster",saved_feature_name="label", target_attr="obs")
+mdata = data_infor_integrate(mdata, feature_key="lsi_pca",saved_feature_name="Unintegrated", target_attr="obsm", dim_limit=100)
 
-mdata = mu.read("./data/pbmc.h5mu")
-mdata.obs["batch"] = mdata.mod["protein"].obs["batch"].loc[mdata.obs_names]
-gmi_model = GraphMosaicIntegration()
+#设定参数
+label_smoothing = 0.1
+alpha = 0.1
+loss_alpha = 0.2
+neg_sampling_mode = "matched"
+
+
+
+gmi_model = GraphMosaicIntegration(
+    label_smoothing=label_smoothing,
+    alpha=alpha,
+    loss_alpha=loss_alpha,
+    neg_sampling_mode=neg_sampling_mode,
+    
+)
 gmi_model.fit(mdata, batch_key="batch", feature_interaction_key="net")
-gmi_model.save(f"./result/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+gmi_model.save(result_path) 
 
-# graph = GraphMosaicIntegration.mdata2graph(
-#     mdata, batch_key="batch", feature_interaction_key="net"
-# )
-# graph.save("./data/graph.h5")
-# graph = MosaicDataGraph.load("./data/graph.h5")
-# print(graph)
+plot_umap(
+    mdata,
+    result_path,
+    neg_sampling_mode=neg_sampling_mode,
+)
+
+run_benchmark(
+    mdata,
+    17055,
+    result_path,
+)
