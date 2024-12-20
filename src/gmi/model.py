fix: calculation of neg_scores, move n_cell of model to __init__

src/gmi/model.py
@@ -94,9 +94,17 @@ class FullModel(nn.Module):
         hidden_dims: tuple[int] = (64,),
         bn: bool = False,
         add_batch_embedding: bool = False,
+        n_cells: int | None = None,
     ):
+        if add_batch_embedding and num_batch is None:
+            raise ValueError(
+                "num_batch must be provided "
+                "when add_batch_embedding is True"
+            )
+
         super(FullModel, self).__init__()
         self.add_batch_embedding = add_batch_embedding
+        self.n_cells = n_cells
 
         # 嵌入层
         self.node_embedding = nn.Embedding(num_nodes, embedding_dim)
@@ -118,7 +126,6 @@ class FullModel(nn.Module):
         neg_edges: torch.Tensor,
         pos_domain_input: torch.Tensor | None = None,
         node_batch_indice: torch.Tensor | None = None,
-        num_cells: int | None = None,
         alpha: float = 1.0,
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
         # 正样本和负样本嵌入
@@ -129,7 +136,7 @@ class FullModel(nn.Module):
 
         if self.add_batch_embedding:
             # 只给cell nodes加batch embedding
-            mask_pos = pos_edges < num_cells
+            mask_pos = pos_edges < self.n_cells
             cell_nodes = pos_edges[mask_pos]
             batch_embeddings = self.batch_embedding(
                 node_batch_indice[cell_nodes]
@@ -138,7 +145,7 @@ class FullModel(nn.Module):
                 pos_edges_emb[mask_pos] + batch_embeddings
             )
 
-            mask_neg = neg_edges < num_cells
+            mask_neg = neg_edges < self.n_cells
             cell_nodes = neg_edges[mask_neg]
             batch_embeddings = self.batch_embedding(
                 node_batch_indice[cell_nodes]
@@ -148,11 +155,11 @@ class FullModel(nn.Module):
             )
 
         # 计算边得分
-        pos_scores = (pos_edges_emb[:, 0, :] * pos_edges_emb[:, 1, :]).sum(
-            dim=-1
+        pos_scores = torch.einsum(
+            "ij,ij->i", pos_edges_emb[:, 0], pos_edges_emb[:, 1]
         )
-        neg_scores = (neg_edges_emb[:, 0, :] * neg_edges_emb[:, 1, :]).sum(
-            dim=-1
+        neg_scores = torch.einsum(
+            "ijk,ijk->ij", neg_edges_emb[:, :, 0], neg_edges_emb[:, :, 1]
         )
 
         # 领域分类嵌入表示

src/gmi/trainer.py
@@ -113,6 +113,7 @@ class Trainer:
         late_join_alpha: int | None = None,
         patience: int | float = 5,  # inf or np.inf表示不使用早停
         random_seed: int | None = None,
+        std_loss_alpha: float = 0.0,
     ):
         self.device = torch.device(device)
         self.neg_sample_in_batch = neg_sample_in_batch
@@ -128,6 +129,7 @@ class Trainer:
         self.late_join_loss_alpha = late_join_loss_alpha
         self.late_join_alpha = late_join_alpha
         self.random_seed = random_seed
+        self.std_loss_alpha = std_loss_alpha
 
         if adversarial_with_feature_nodes:
             raise NotImplementedError(
@@ -172,7 +174,6 @@ class Trainer:
                 else None,
                 node_batch_indice=batch["node_groups"],
                 alpha=alpha,
-                num_cells=self.graph.n_cells,
             )
             # 计算当前批次的损失
             loss, loss_dict = graph_mosaic_integration_loss(
@@ -193,6 +194,7 @@ class Trainer:
                 edge_loss_type=self.loss_type,
                 loss_alpha=loss_alpha,
                 label_smoothing=self.label_smoothing,
+                std_loss_alpha=self.std_loss_alpha,
             )
 
             # 反向传播与优化
@@ -218,7 +220,6 @@ class Trainer:
                     batch["pos_edges"],
                     batch["neg_edges"],
                     node_batch_indice=batch["node_groups"],
-                    num_cells=self.graph.n_cells,
                 )
                 # 前向传播
                 # 前向计算损失